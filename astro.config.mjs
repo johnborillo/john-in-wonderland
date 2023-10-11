5351c9c5eaa491db799101e5bcdd2a7dd7662bbc
John Borillo <73507954+johnborillo@users.noreply.github.com>
2023-10-10 23:24:14 -0400

    Added requirements for github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://johnborillo.github.io',
+	base: '/john-in-wonderland',
 	integrations: [mdx(), sitemap()],
 });
