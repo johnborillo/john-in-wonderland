1125a28bb31f33d57076f5bc71b553d2bf8883d9
John Borillo <73507954+johnborillo@users.noreply.github.com>
2023-10-15 21:58:22 -0400

    Still fixing navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://johnborillo.github.io/john-in-wonderland',
-	// base: '/john-in-wonderland/',
+	site: 'https://johnborillo.github.io',
+	base: '/john-in-wonderland/',
 	integrations: [mdx(), sitemap()],
-	
-	// Define routes for your pages
-	// routes: [
-	// 	{ src: '/home', dest: '/src/routes/home.astro' },
-	//   ],
 });
